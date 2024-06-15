8b8ae3b6125255140b755568af07a9adf77eef0b
A P <131835703+s81c8f@users.noreply.github.com>
2024-06-15 17:28:23 -0400

    Create to-do object and folder object

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
 import './normalize.css';
 import './style.css';
+
+function createTodoItem(title, description, dueDate, priority, notes = '', checklist = []) {
+    return {
+        title,
+        description,
+        dueDate,
+        priority,
+        notes,
+    };
+}
+
+function createProjectManager() {
+    const projectsFolder = {
+        'default': [],
+    }
+
+    return {
+        getProjectsFolder() {
+            return projectsFolder;
+        },
+
+        addProject(name) {
+            if(!projectsFolder[name]) {
+                projectsFolder[name] = [];
+            }
+        },
+
+        addTodo(project, todo) {
+            if(project) {
+                projectsFolder[project].push(todo);
+            }
+        },
+
+        getTodos(project) {
+            return projectsFolder[project] ? projectsFolder[project] : [];
+        }
+    }
+}
